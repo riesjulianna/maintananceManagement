777b7a3ab7d2ec22bdbb77455b2e092aa09b4749
riesjulianna <a.riesjulianna@gmail.com>
2022-03-06 21:46:36 +0100

    firebase almost done

--- app/src/main/java/com/example/mm/MainActivity.java
+++ app/src/main/java/com/example/mm/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.mm;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button getBtn, sendBtn;
+    TextView testText;
+    EditText testData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testData = findViewById(R.id.editText);
+        sendBtn = findViewById(R.id.buttonSend);
+        getBtn = findViewById(R.id.buttonGet);
+        testText = findViewById(R.id.textView2);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //send to db
+            }
+        });
+
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //read from db
+            }
+        });
     }
 }
