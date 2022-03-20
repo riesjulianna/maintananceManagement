c218b961bcbfabed3354bb4996831796eb92731d
riesjulianna <a.riesjulianna@gmail.com>
2022-03-20 14:41:33 +0100

    all layouts and navigation between activities

---
+++ app/src/main/java/com/example/mm/LordSzerkeszt.java
@@ -0,0 +1,14 @@
+package com.example.mm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LordSzerkeszt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lord_szerkeszt);
+    }
+}
