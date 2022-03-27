2a6163759f5f7d83d2972b4789ac8c7e30f37dfc
riesjulianna <a.riesjulianna@gmail.com>
2022-03-27 22:07:24 +0200

    login fixed

--- app/src/main/java/com/example/mm/MainActivity.java
+++ app/src/main/java/com/example/mm/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mm;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,15 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -59,44 +53,31 @@ public class MainActivity extends AppCompatActivity {
                 .whereEqualTo("felhasznalonev", fnev.getText().toString().trim())
                 .whereEqualTo("jelszo", jelszo.getText().toString().trim())
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            beosztas = document.getString("beosztas");
-                            fhnv = document.getString("felhasznalonev");
-                            jlsz = document.getString("jelszo");
-
-                            Toast.makeText(MainActivity.this, "lekerdezes kesz:"+beosztas, Toast.LENGTH_SHORT).show();
-
-                            if (fnev.getText().toString().trim().equals(fhnv)
-                                    && jelszo.getText().toString().trim().equals(jlsz)) {
-                                switch (view.getId()) {
-                                    case R.id.buttonLord:
-                                        Intent lord = new Intent(MainActivity.this, LordMainActivity.class);
-                                        startActivity(lord);
-                                        break;
-                                    case R.id.buttonHazvezetono:
-                                        Intent hazvezetono = new Intent(MainActivity.this, HnoMainActivity.class);
-                                        startActivity(hazvezetono);
-                                        break;
-                                    case R.id.buttonSzolga:
-                                        Intent szolga = new Intent(MainActivity.this, SzolgaMainActivity.class);
-                                        startActivity(szolga);
-                                        break;
-                                }
-                            }else{
-                                Toast.makeText(MainActivity.this, "bla", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    } else {
-                        Toast.makeText(MainActivity.this, "nem", Toast.LENGTH_SHORT).show();
+                .addOnCompleteListener(task -> {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        beosztas = document.getString("beosztas");
+                        fhnv = document.getString("felhasznalonev");
+                        jlsz = document.getString("jelszo");
                     }
-                }
-
-
-    });
+                        if (!(beosztas==null)) { // itt mindegyik null ha nem stimmel, ezért csak egyet ellenőrzök
+                            switch (view.getId()) {
+                                case R.id.buttonLord:
+                                    Intent lord = new Intent(MainActivity.this, LordMainActivity.class);
+                                    startActivity(lord);
+                                    break;
+                                case R.id.buttonHazvezetono:
+                                    Intent hazvezetono = new Intent(MainActivity.this, HnoMainActivity.class);
+                                    startActivity(hazvezetono);
+                                    break;
+                                case R.id.buttonSzolga:
+                                    Intent szolga = new Intent(MainActivity.this, SzolgaMainActivity.class);
+                                    startActivity(szolga);
+                                    break;
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "Helytelen valami.", Toast.LENGTH_SHORT).show();
+                        }
+                });
     }
 
     @Override
