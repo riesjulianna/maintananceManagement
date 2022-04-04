dfec5ebcb6b768d812a85eab131904cb4fd7324e
ihmate <89023847+ihmate@users.noreply.github.com>
2022-04-04 21:06:48 +0200

    Lord eszköz hozzáadás átalakítva, uniqueID, egyezőség ellenőrzése félig kész

--- app/src/main/java/com/example/mm/LordEszkozLista.java
+++ app/src/main/java/com/example/mm/LordEszkozLista.java
@@ -3,8 +3,14 @@ package com.example.mm;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
