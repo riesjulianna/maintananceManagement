efe034a05de6bcac0b6909899ab6a3345476d982
anett <tucz13@gmail.com>
2022-04-30 21:31:07 +0200

    hibajelentes activity rendkivüli karbantartások rogzitése a feladatok kozé (eszköz,idopont,hibaleiras)

--- app/src/main/java/com/example/mm/Hibajelentes.java
+++ app/src/main/java/com/example/mm/Hibajelentes.java
@@ -2,34 +2,143 @@ package com.example.mm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.ArrayMap;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Hibajelentes extends AppCompatActivity implements View.OnClickListener {
+import com.google.firebase.Timestamp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    Button leadas;
+
+public class Hibajelentes extends AppCompatActivity  {
+
+    Button mentes;
+    EditText problema;
+    Spinner nev;
+    List<String> list, listnev;
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hibajelentes);
 
-        leadas = findViewById(R.id.buttonHibaBejelentes);
-        leadas.setOnClickListener(this);
-    }
+        mentes = findViewById(R.id.buttonHibaBejelentes);
+        problema = findViewById(R.id.problema);
+        nev = findViewById(R.id.spinner);
 
 
-    @Override
-    public void onClick(View view) {//sztem itt is dupla klikkes lesz
-        switch (view.getId()) {
-            case R.id.buttonHibaBejelentes:
-                //hiba leadás, feladat generálás
-                Intent i = new Intent(this, SzolgaMainActivity.class);
-                startActivity(i);
+        listnev = new ArrayList<>();
+        list= new ArrayList<>();
+
+
+
+
+        DatabaseReference ref = db.getReference().child("eszkozok");
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    String id = ds.getKey();
+                    String n = snapshot.child(id).child("nev").getValue().toString();
+                    list.add(id);
+
+                    listnev.add(n);
+                }
+
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(Hibajelentes.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listnev);
+                adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+                nev.setAdapter(adapter);
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        ref.addListenerForSingleValueEvent(eventListener);
+
+
+
+
+
+
+
+
+        mentes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nevid, problem, idnev = null, id;
+
+                nevid = nev.getSelectedItem().toString();
+                problem = problema.getText().toString();
+                String currentDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+                for (int y=0; y< list.size(); y++){
+                    if (listnev.get(y) == nevid)
+                    {
+                        idnev = list.get(y);
+                    }
+
+                }
+
+                //unique ID generálása
+                id = UUID.randomUUID().toString();
+                //egyező id ellenőrzés
+                DatabaseReference ref = db.getReference().child("eszkozok");
+                ValueEventListener eventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot ds : snapshot.getChildren()) {
+                            String id = ds.getKey();
+                            Log.d("KEY---", id);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                };
+                ref.addListenerForSingleValueEvent(eventListener);
+
+
+                DatabaseReference  refi= db.getReference().child("feladatok");
+                refi.child(id).child("allapot").setValue("Új");
+                refi.child(id).child("eszkozID").setValue(idnev);
+                refi.child(id).child("problema").setValue(problem);
+                refi.child(id).child("tipus").setValue("rendkivuli");
+                refi.child(id).child("date").setValue(currentDate);
                 onBackPressed();
-                break;
-        }
+            }
+        });
     }
+
 }
